ceb4c0054fc58cc331bb3407efa52ea64e81e7ac
Karl <krf0821@gmail.com>
2025-04-21 19:54:20 +0100

    Added skeleton code for fitness app

---
+++ momentum/app/firebase/config.ts
@@ -0,0 +1,29 @@
+// firebase/config.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth, getReactNativePersistence } from "firebase/auth/react-native";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCwDlN6bQgu9ayrCyN4_U6ylDvMjh17Rdc",
+    authDomain: "momentum0821.firebaseapp.com",
+    projectId: "momentum0821",
+    storageBucket: "momentum0821.firebasestorage.app",
+    messagingSenderId: "443859253397",
+    appId: "1:443859253397:web:a16ae5de21280e7e92039c",
+    measurementId: "G-5CEW5L0TSC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Auth & Firestore exports
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+const db = getFirestore(app);
+
+export { auth, db };
